a8c20670952cd7d9e4a8f00b0c70099f4bd98a02
ChowChowSonic <joeyant1367@gmail.com>
2025-03-12 13:56:22 -0400

    added logging on error to main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """Main (driver) code """
 
+import logging
 import sys
 from app import App
 
@@ -27,11 +28,16 @@ def calculate_and_print(argv: list[str] = []) -> float | int:
             app.execute_command(argv[-1], argv[:-1])
     except ZeroDivisionError:
         print("An error occurred: Cannot divide by zero")
+        logging.error("Attempted to divide by zero. Aborting...")
     except KeyError:
         print("Unknown operation:", argv[-1] if len(argv) > 0 else lst[-1])
+        logging.error("Unknown operation passed: %s; Aborting...", argv[-1] if len(argv) > 0 else lst[-1])
     except ValueError:
         print(f"Invalid number input: {argv[0]} or {argv[1]} is not a valid number.")
+        logging.error("Invalid number input entered: %i %i; Aborting...", argv[0], argv[1])
     except EOFError:
+        # This is to be expected when we pipe stuff in. This is intended
+        # behavior so there will be no errors to be logged because of that
         pass
 
 if __name__ == "__main__":
