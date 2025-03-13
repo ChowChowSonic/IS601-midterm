f281be9a1462546f5f05a87ce4689766215af2f8
ChowChowSonic <joeyant1367@gmail.com>
2025-03-13 14:19:35 -0400

    moved env to true singleton implementation

--- app/env/__init__.py
+++ app/env/__init__.py
@@ -2,14 +2,9 @@
 import os
 import logging 
 from dotenv import load_dotenv
+from app.singleton import Singleton 
 
-class Env:
-    _instance = None
-    def __new__(class_, *args, **kwargs):
-        if not isinstance(class_._instance, class_):
-            class_._instance = object.__new__(class_, *args, **kwargs)
-        return class_._instance
-
+class Env(Singleton):
     def __init__(self):
         load_dotenv()
         self.settings = dict(os.environ.items())
@@ -19,9 +14,3 @@ class Env:
     def getenv(name:str):
         """Gets an environment variable from the env instance"""
         return Env.get_instance().settings[name]
-
-    def get_instance(): 
-        """Returns the env instance""" 
-        if Env._instance is None: 
-            Env._instance = Env() 
-        return Env._instance
